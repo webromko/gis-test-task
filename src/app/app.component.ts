90cf41408a76760921d15245c2ebb1fec3798e12
webromko <mylogin222@gmail.com>
2023-05-11 22:53:46 +0300

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'gis-test-task';
+
+  constructor() {
+    setTheme('bs4'); // manually specify bootstrap version
+  }
 }
